6ca57fe9a5e7a721f758138e12dfaece04b03dc7
faarahsaed <faarahsaed87@gmail.com>
2023-02-19 12:13:16 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
 /**
- * main - A program that prints the zise of  various types  
- * Return: 0 (Success)
- */
+*main - main function
+*
+*
+*Return: return 0
+*/
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float f;
-
-printf("Size of a char: %lu byte(s) \n", (unsigned long)siz
-eof(a));
-printf("Size of an int: %lu byte(s) \n", (unsigned long)siz
-eof(b));
-printf("Size of a long int: %lu bytes (s)\n", (unsigned lon
-g)sizeof(c));
-printf("Size of a long long int: %lu bytes (s) \n", (unsigne
-d long)sizeof(d));
-printf("Size of a float: %lu bytes (s)\n", (unsigned long)s
-isof(f));
-return (0);
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
