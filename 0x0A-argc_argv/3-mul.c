1ebdde9121741ba55b4a6daa491ce287469b1026
faarahsaed <faarahsaed87@gmail.com>
2023-03-10 22:09:00 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,7 +3,6 @@
 /**
 * _atoi - converts a string to an integer
 * @s: string to be converted
-*
 * Return: the int converted from the string
 */
 int _atoi(char *s)
@@ -41,10 +40,10 @@ return (n);
 /**
 * main - multiplies two numbers
 * @argc: number of arguments
-* @argv: array of arguments 
+* @argv: array of arguments
 * Return: 0 (Success), 1 (Error)
 */
-int main(int argc, char *argv[])
+int main(int argc, char argv[])
 {
 int result, num1, num2;
 if (argc < 3 || argc > 3)
@@ -54,7 +53,7 @@ return (1);
 }
 num1 = _atoi(argv[1]);
 num2 = _atoi(argv[2]);
-result = num1 * num2;
+result = num1 *  num2;
 printf("%d\n", result);
 return (0);
 }
